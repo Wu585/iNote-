1ff8dbf8023fc797ef081812366aef39fd12217b
Mike <15151851516@163.com>
2021-06-04 15:26:35 +0800

    引入 less，添加 avatar 组件

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="sidebar">
+    <Avatar/>
     <div class="icons">
       <router-link to="/note/1" title="笔记"><i class="iconfont icon-note"></i></router-link>
       <router-link to="/notebooks" title="笔记本"><i class="iconfont icon-notebook"></i></router-link>
@@ -11,30 +12,31 @@
   </div>
 </template>
 <script>
-export default {}
+import Avatar from "@/components/Avatar";
+export default {
+  components: {Avatar}
+}
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 #sidebar {
   position: relative;
   width: 56px;
   text-align: center;
   background-color: #2c333c;
 }
-
 .icons {
   margin-top: 15px;
-}
 
-.icons a {
-  padding: 6px 0;
-  display: block;
+  a {
+    padding: 6px 0;
+    display: block;
+  }
 }
 
 .icons .router-link-active {
   background-color: #5e6266;
 }
-
 .logout {
   position: absolute;
   bottom: 20px;
@@ -42,7 +44,6 @@ export default {}
   text-align: center;
   cursor: pointer;
 }
-
 .iconfont {
   color: #fff;
 }
