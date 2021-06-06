f81d4061de9e06ff25279a4bb8099ee9322f42bb
Mike <15151851516@163.com>
2021-06-07 02:14:06 +0800

    笔记列表更新

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,24 +1,59 @@
 <template>
-  <div>
-    <h1>{{ msg }}:{{ $route.params.noteId }}</h1>
+  <div id="note" class="detail">
+    <NoteSideBar  @update:notes="val=>notes=val"/>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span>创建日期：{{ curNote.createdAtFriendly }}</span>
+        <span>更新日期：{{ curNote.updatedAtFriendly }}</span>
+        <span>{{ curNote.statusText }}</span>
+        <span class="iconfont icon-delete"></span>
+        <span class="iconfont icon-fullscreen"></span>
+      </div>
+      <div class="note-title">
+        <input type="text" v-model="curNote.title" placeholder="输入标题">
+      </div>
+      <div class="editor">
+        <textarea v-show="true" :value="curNote.content" placeholder="输入内容，支持 markdown 语法"></textarea>
+        <div class="preview markdown-body" v-show="false"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NoteSideBar from "@/components/NoteSideBar";
+import Auth from '@/apis/auth'
+
 export default {
-  name: 'Login',
+  components: {NoteSideBar},
   data() {
     return {
-      msg: '笔记本详情页'
+      curNote: {},
+      notes: []
     }
   },
-  methods: {
-    open() {
-      this.$message({
-        message: '提示',
-        type: 'success'
-      })
-    }
+  created() {
+    Auth.getInfo()
+        .then(res => {
+          if (!res.isLogin) {
+            this.$router.push({path: '/login'})
+          }
+        })
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.curNote = this.notes.find(note => note.id == to.query.noteId)  //this.curNote是一个执行this.notes里的对应那项的指针
+    console.log(this.curNote);
+    next()
   }
 }
 </script>
+<style lang="less">
+@import url(../assets/css/note-detail.less);
+
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
+}
+</style>
